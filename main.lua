7de98876bdf78034d19fc13eaac11383fc4e9545
LucaMastrofini <lucam96@yahoo.it>
2025-09-01 15:37:10 +0200

    adding animations on movement

--- main.lua
+++ main.lua
@@ -1,12 +1,26 @@
 function love.load()
+    anim8 = require 'libraries/anim8'
+    love.graphics.setDefaultFilter('nearest', 'nearest') -- set nearest neighbor filtering on upscaling and downscaling to prevent blurriness
     player = {}
     player.x = 400
     player.y = 300
     player.speed = 3
-    --player.acceleration = 0.1
-    player.sprite = love.graphics.newImage("sprites/parrot.png")
+    -- player.acceleration = 0.1
+    -- player.sprite = love.graphics.newImage("sprites/parrot.png")
+    player.spriteSheet = love.graphics.newImage("sprites/player-sheet.png")  -- loading the sprite sheet (in this case the player's walking animations)
+    background = love.graphics.newImage("sprites/background.png") -- loading the background image
+    -- creating a grid for the sprite sheet 12x18 is the size of each frame in the sprite sheet    
+    player.grid = anim8.newGrid(12,18,player.spriteSheet:getWidth(), player.spriteSheet:getHeight())    
+    player.animations = {}
 
-    background = love.graphics.newImage("sprites/background.png")
+    -- defining the animations for each direction
+    player.animations.down = anim8.newAnimation(player.grid('1-4', 1), 0.2)
+    player.animations.left = anim8.newAnimation(player.grid('1-4', 2), 0.2)
+    player.animations.right = anim8.newAnimation(player.grid('1-4', 3), 0.2)
+    player.animations.up = anim8.newAnimation(player.grid('1-4', 4), 0.2)
+
+    -- defing variable to switch between animations
+    player.anim = player.animations.down
 end
 
 function love.update(dt)
@@ -15,28 +29,36 @@ function love.update(dt)
         --player.speed = player.speed + player.acceleration
         player.x = player.x + player.speed
         isMoving = true
+        player.anim = player.animations.right
     end
     if love.keyboard.isDown("left") then
         --player.speed = player.speed + player.acceleration
         player.x = player.x - player.speed
         isMoving = true
+        player.anim = player.animations.left
+        
     end
     if love.keyboard.isDown("down") then
         --player.speed = player.speed + player.acceleration
         player.y = player.y + player.speed
         isMoving = true
+        player.anim = player.animations.down
     end
     if love.keyboard.isDown("up") then
         --player.speed = player.speed + player.acceleration
         player.y = player.y - player.speed
         isMoving = true
+        player.anim = player.animations.up
     end
     if not isMoving then
-        --player.speed = 3
+        player.anim:gotoFrame(2) 
     end
+    player.anim:update(dt)
+
+     -- update the animation (dt is delta time, the time elapsed since the last frame)
 end
 
 function love.draw()
     love.graphics.draw(background, 0, 0)
-    love.graphics.draw(player.sprite, player.x, player.y)   
+    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 10)  -- source / position x / position y / rotation / scale x / scale y
 end 
