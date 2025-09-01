d73b2eadb6a9ba65e1c56ed1b1ad07ae628af4e6
LucaMastrofini <lucam96@yahoo.it>
2025-09-01 20:53:39 +0200

    adding camera

--- main.lua
+++ main.lua
@@ -1,10 +1,12 @@
 function love.load()
+    camera = require 'libraries/camera' 
+    cam = camera() -- include the camera library
     anim8 = require 'libraries/anim8'
     sti = require 'libraries/sti'
     gameMap = sti('maps/mapDemo.lua')  -- loading the map created with Tiled
     love.graphics.setDefaultFilter('nearest', 'nearest') -- set nearest neighbor filtering on upscaling and downscaling to prevent blurriness
 
-    
+
     player = {}
     player.x = 400
     player.y = 300
@@ -58,11 +60,31 @@ function love.update(dt)
         player.anim:gotoFrame(2) 
     end
     player.anim:update(dt)
+    cam:lookAt(player.x, player.y)  -- make the camera follow the player
 
+    local w = love.graphics.getWidth()
+    local h = love.graphics.getHeight()
+    if cam.x < w/2 then
+        cam.x = w/2
+    end
+    if cam.y < h/2 then
+        cam.y = h/2
+    end
+    local mapW = gameMap.width * gameMap.tilewidth
+    local mapH = gameMap.height * gameMap.tileheight
+    if cam.x > (mapW - w/2) then
+        cam.x = (mapW - w/2)
+    end
+    if cam.y > (mapH - h/2) then
+        cam.y = (mapH - h/2)   
+    end
      -- update the animation (dt is delta time, the time elapsed since the last frame)
 end
 
 function love.draw()
-    gameMap:draw()
-    player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6)  -- source / position x / position y / rotation / scale x / scale y
+    cam:attach()  -- begin camera
+        gameMap:drawLayer(gameMap.layers["Ground"])  -- drawing the map layer named "Tile Layer 1"
+        gameMap:drawLayer(gameMap.layers["Trees"]) -- drawing the map layer named "Trees"
+        player.anim:draw(player.spriteSheet, player.x, player.y, nil, 6, nil, 6, 9)  -- source image, x, y, rotation, scale x, scale y, offset x, offset y
+    cam:detach()  -- end camera
 end 
